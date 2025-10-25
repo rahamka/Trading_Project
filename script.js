0f0b8549a2756d3ab7783104f2e7bf960e241b96
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-10-25 20:25:45 +0500

    I completed cardHideNumbers Problem.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function hideCreditCard(cardNum) {
-  let value = cardNum;
+  cardNum = cardNum.toString();
+  // I convert cardNum toString but before in which condition it was?
+  let value = "";
+  for (let i = 0; i <= 11; i++) {
+    value += cardNum[i];
+  }
+  value += "****";
+  console.log(value);
 }
 
-hideCreditCard(1234567891234567);
+hideCreditCard("rahamkalhoro1280");
