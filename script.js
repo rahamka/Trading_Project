1c6f5ea86ad302ee4d51e01a88e6f193f5d50845
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-10-28 11:59:33 +0500

    Added Toggle Button.

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-function hideCreditCard(cardNum) {
-  cardNum = cardNum.toString();
-  // I convert cardNum toString but before in which condition it was?
-  let value = "";
-  for (let i = 0; i <= 11; i++) {
-    value += cardNum[i];
-  }
-  value += "****";
-  console.log(value);
-}
+let modeBtn = document.getElementById("modeDiv");
+let container = document.getElementById("container");
+let featureBtn = document.getElementById("features-container");
+// here querySelectorAll isn't working?
+let cards = document.querySelectorAll(".card");
+modeBtn.addEventListener("click", () => {
+  // modeBtn innerText
+  if (modeBtn.innerText == "Dark Mode") {
+    container.style.background = "rgba(112, 112, 112, 1)";
+    container.style.color = "white";
+    featureBtn.style.background = "white";
+    modeBtn.style.background = "white";
+    featureBtn.style.color = "black";
+    modeBtn.style.color = "black";
+    modeBtn.innerText = "Light Mode";
 
-hideCreditCard("rahamkalhoro1280");
+    // loop through each card
+    cards.forEach((card) => {
+      card.style.backgroundColor = "rgba(75, 75, 75, 1)";
+      card.style.color = "white";
+    });
+  } else if (modeBtn.innerText == "Light Mode") {
+    modeBtn.innerText = "Dark Mode";
+    container.style.background = "#ffffffc5";
+    container.style.color = "black";
+    featureBtn.style.background = "black";
+    featureBtn.style.color = "white";
+    modeBtn.style.color = "white";
+    modeBtn.style.backgroundColor = "black";
+
+    // loop through each card
+    cards.forEach((card) => {
+      card.style.backgroundColor = "#fff";
+      card.style.color = "black";
+    });
+  }
+});
+
+// Note in this code querySelectorAll isn't working because of that cards color isn't changing?
+// fix this error, teach me in simple words why querySelectorAll isn't working?
